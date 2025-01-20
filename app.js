dc3c1641940a273292d671ea5782b806c65c7e96
wsfer <93054415+wsfer@users.noreply.github.com>
2025-01-20 15:57:31 -0300

    chore: add prettier

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.port || 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get("/", (req, res) => res.send("Hello"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
