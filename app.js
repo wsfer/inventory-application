ff0c67053611a7e8fab2004ca267817b9cdb29a3
wsfer <93054415+wsfer@users.noreply.github.com>
2025-01-22 10:03:14 -0300

    feat: add config to serve static files

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = process.env.port || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.render("home"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
