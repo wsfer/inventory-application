8fdbcba9d0956e7b099780d160c6b8c55b93a427
wsfer <93054415+wsfer@users.noreply.github.com>
2025-01-22 15:22:07 -0300

    feat: add routing endpoints to routers on main file

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRouter");
+const gameRouter = require("./routes/gameRouter");
+const genreRouter = require("./routes/genreRouter");
+
 const PORT = process.env.port || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.use("/games", gameRouter);
+app.use("/genre", genreRouter);
+app.use("/", indexRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
