d65cbbcef1acc42730585814989bdb12c8b799ad
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-10 21:51:53 -0300

    feat: add query to get recent games from database

--- db/gameQueries.js
+++ db/gameQueries.js
@@ -1,8 +1,19 @@
 const pool = require("./pool");
 
 async function getAll() {
-  const { rows } = pool.query("SELECT * FROM game");
+  const { rows } = await pool.query("SELECT * FROM game");
   return rows;
 }
 
-module.exports = { getAll };
+// TODO: limit results to 5 and order by created_at
+async function getRecent() {
+  const { rows } = await pool.query(`
+    SELECT game.*, json_object_agg(genre.id, genre.name) AS genre FROM game
+    JOIN game_genre ON game.id = game_genre.game_id
+    JOIN genre ON genre_id = game_genre.genre_id
+    GROUP BY game.id;
+  `);
+  return rows;
+}
+
+module.exports = { getAll, getRecent };
