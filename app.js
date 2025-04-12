2c41b19994a99d743caccf819739072796b7a342
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-11 22:34:25 -0300

    feat: add error handler middleware

--- app.js
+++ app.js
@@ -13,4 +13,12 @@ app.use("/games", gameRouter);
 app.use("/genre", genreRouter);
 app.use("/", indexRouter);
 
+app.use((err, req, res, next) => {
+  if (err.name === "NotFoundError") {
+    res.render("404", { error: err });
+  } else {
+    res.status(500).end();
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
