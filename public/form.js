cdda79c6d7617815acbcb64d332a6d869fcfd987
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-22 22:27:13 -0300

    feat: add error message when image can't be loaded on game form (client side only)

--- public/form.js
+++ public/form.js
@@ -1,14 +1,29 @@
 (function addPreviewImageScript() {
   const imageInput = document.querySelector(".js-image-input");
   const image = document.querySelector(".js-image");
-  const placeholder = image.src; // Initial placeholder
+  const errorContainer = document.querySelector(".js-image-errors");
+  const loadingErrorMessage = new Range().createContextualFragment(
+    "<li>Error loading image, check if the url is correct</li>",
+  ).firstChild;
 
-  // Useful after page reload
+  // Keep a reference to the placeholder image source
+  const placeholder = image.src;
+
+  // Used on page reload to set image to the input value
   if (imageInput.value) {
     image.src = imageInput.value;
   }
 
   imageInput.addEventListener("blur", (e) => {
+    // Remove error message when it's displayed
+    if (loadingErrorMessage.parentElement === errorContainer) {
+      errorContainer.removeChild(loadingErrorMessage);
+    }
     image.src = e.target.value === "" ? placeholder : e.target.value;
+  });
+
+  image.addEventListener("error", (e) => {
+    errorContainer.appendChild(loadingErrorMessage);
+    image.src = placeholder;
   });
 })();
