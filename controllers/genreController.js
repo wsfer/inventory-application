7ad580cd3ba504a02e6947b0f78a933da0da3bd4
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-24 22:13:50 -0300

    feat: add input validation check for genre creation on genre controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,13 +1,26 @@
+const { validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const genreQueries = require("../db/genreQueries");
+const validateGenre = require("../middlewares/validateGenre");
 
 const getGenreForm = asyncHandler(async (req, res) => {
   const genres = await genreQueries.getAll();
   res.render("genre");
 });
 
-const createGenre = asyncHandler(async (req, res) => {
-  res.redirect("/create");
-});
+// TODO: check if genre name already exists
+const createGenre = [
+  validateGenre,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const errorMessage = errors.array()[0].msg;
+      return res.status(400).render("genre", { error: errorMessage });
+    }
+
+    res.redirect("/create");
+  }),
+];
 
 module.exports = { getGenreForm, createGenre };
