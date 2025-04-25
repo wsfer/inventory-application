5fcd24acfabd3f8aa0e20246b47c6c04caeccf09
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-24 22:35:36 -0300

    feat: add modal to show all genres on genre form page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -5,7 +5,7 @@ const validateGenre = require("../middlewares/validateGenre");
 
 const getGenreForm = asyncHandler(async (req, res) => {
   const genres = await genreQueries.getAll();
-  res.render("genre");
+  res.render("genre", { genres: genres });
 });
 
 // TODO: check if genre name already exists
@@ -13,10 +13,13 @@ const createGenre = [
   validateGenre,
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
+    const genres = await genreQueries.getAll();
 
     if (!errors.isEmpty()) {
       const errorMessage = errors.array()[0].msg;
-      return res.status(400).render("genre", { error: errorMessage });
+      return res
+        .status(400)
+        .render("genre", { genres: genres, error: errorMessage });
     }
 
     res.redirect("/create");
---
+++ public/genre.js
@@ -0,0 +1,3 @@
+document.querySelector(".js-show-modal-btn").addEventListener("click", () => {
+  document.querySelector(".js-modal").showModal();
+});
