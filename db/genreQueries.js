d680b45f9792a006304f13dc25e6ade68ccd4d7e
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-24 22:49:31 -0300

    feat: create database query to get genre by name

--- db/genreQueries.js
+++ db/genreQueries.js
@@ -5,8 +5,16 @@ async function getAll() {
   return rows;
 }
 
+async function getByName(name) {
+  const { rows } = await pool.query(
+    "SELECT * FROM genre WHERE LOWER(name) = LOWER($1)",
+    [name],
+  );
+  return rows;
+}
+
 async function createGenre(name) {
   await pool.query("INSERT INTO genre (name) VALUES ($1)", [name]);
 }
 
-module.exports = { getAll, createGenre };
+module.exports = { getAll, getByName, createGenre };
