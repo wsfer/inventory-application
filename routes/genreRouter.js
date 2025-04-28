f943899a4ba4aca3670568a4e0792a7663c07d7d
wsfer <93054415+wsfer@users.noreply.github.com>
2025-04-27 23:08:32 -0300

    feat: add delete genre route

--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -5,5 +5,6 @@ const genreRouter = Router();
 
 genreRouter.get("/create", genreController.getGenreForm);
 genreRouter.post("/create", genreController.createGenre);
+genreRouter.post("/delete/:id", genreController.deleteGenre);
 
 module.exports = genreRouter;
